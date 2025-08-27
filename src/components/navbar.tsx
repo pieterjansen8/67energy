a273fa9d68e2af5429055e0f5532cdf7dd49115f
pieterjansen8 <pieterjansen8@icloud.com>
2025-08-27 21:42:36 +0200

    1.0

---
+++ src/components/navbar.tsx
@@ -0,0 +1,159 @@
+"use client";
+import { Menu } from "lucide-react";
+import { ReactNode } from "react";
+
+import { cn } from "@/lib/utils";
+
+import LaunchUI from "@/components/logos/launch-ui";
+import { Button, type ButtonProps } from "@/components/ui/button";
+import {
+  Navbar as NavbarComponent,
+  NavbarLeft,
+  NavbarRight,
+} from "@/components/ui/navbar";
+import Navigation from "@/components/ui/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react"
+
+interface NavbarLink {
+  text: string;
+  href: string;
+}
+
+interface NavbarActionProps {
+  text: string;
+  href: string;
+  variant?: ButtonProps["variant"];
+  icon?: ReactNode;
+  iconRight?: ReactNode;
+  isButton?: boolean;
+}
+
+interface NavbarProps {
+  logo?: ReactNode;
+  name?: string;
+  homeUrl?: string;
+  mobileLinks?: NavbarLink[];
+  actions?: NavbarActionProps[];
+  showNavigation?: boolean;
+  customNavigation?: ReactNode;
+  className?: string;
+}
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+export default function Navbar({
+  logo = <LaunchUI />,
+  name = "67 energy",
+  homeUrl = "#",
+  mobileLinks = [
+   
+  ],
+  actions = [
+
+  ],
+  showNavigation = true,
+  customNavigation,
+  className,
+}: NavbarProps)
+  {
+    const { setTheme } = useTheme()
+  return (
+    <header className={cn("sticky top-0 z-50 -mb-4 px-4 pb-4", className)}>
+      <div className="fade-bottom bg-background/15 absolute left-0 h-24 w-full backdrop-blur-lg"></div>
+      <div className="max-w-container relative mx-auto">
+        <NavbarComponent>
+          <NavbarLeft>
+            <a
+              href={homeUrl}
+              className="flex items-center gap-2  text-xl font-bold"
+            >
+              {logo}
+              {name}
+            </a>
+          </NavbarLeft>
+          <NavbarRight>
+            {actions.map((action, index) =>
+              action.isButton ? (
+                <Button
+                  key={index}
+                  variant={action.variant || "default"}
+                  asChild
+                >
+                  <a href={action.href}>
+                    {action.icon}
+                    {action.text}
+                    {action.iconRight}
+                  </a>
+                </Button>
+              ) : (
+                <a
+                  key={index}
+                  href={action.href}
+                  className="hidden text-sm md:block"
+                >
+                  {action.text}
+                </a>
+              ),
+            )}
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="shrink-0 md:hidden"
+                >
+                  <Menu className="size-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="right">
+                <nav className="grid gap-6 text-lg font-medium">
+                  <a
+                    href={homeUrl}
+                    className="flex items-center gap-2 text-xl font-bold"
+                  >
+                    <span>{name}</span>
+                  </a>
+                  {mobileLinks.map((link, index) => (
+                    <a
+                      key={index}
+                      href={link.href}
+                      className="text-muted-foreground hover:text-foreground"
+                    >
+                      {link.text}
+                    </a>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                    </DropdownMenu>
+          </NavbarRight>
+        </NavbarComponent>
+      </div>
+    </header>
+  );
+}
